dd017ae2deee02ce7b7a9987a319903fa3d1cf32
Leonardo Crestani <leonardo.crestani@hotmail.com>
2020-11-05 14:02:14 -0300

    Game update 1

--- game.cpp
+++ game.cpp
@@ -2,25 +2,35 @@
 
 int main()
 {
-    const int windowWidth = 800, windowHeight = 800;
+    const int windowWidth = 1000, windowHeight = 600;
 
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Asteroids Remastered 2020");
     window.setFramerateLimit(60);
 
     sf::Texture backgroundTexture;
-    backgroundTexture.loadFromFile("assets/backgroundGame.jpg");
-    sf::IntRect backgroundRect(0, 0, windowWidth, windowHeight);
+    backgroundTexture.loadFromFile("./assets/backgroundImage.png");
     sf::Sprite backgroundSprite(backgroundTexture);
 
+    // Game loop (funcionamento)
     while (window.isOpen())
     {
+        // Tratamento de eventos
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
+            else if (event.type == sf::Event::KeyPressed) {
+                if(event.key.code == sf::Keyboard::Escape) {
+                    window.close();
+                }
+            }
         }
 
+        // Atualizacao dos estados do jogo (elementos)
+
+        // Local onde sao desenhados os frames
         window.clear();
         window.draw(backgroundSprite);
         window.display();
